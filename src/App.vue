b28166e60fb9ff3a5fee4e7c1d10646bf8713aac
Giovana Assis <giovana.sant@hotmail.com>
2022-11-01 23:52:27 -0300

    Refactor - Components, CLI and Hoocks

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <PrimeiroComponenteVue />
+</template>
+
+
+<script>
+import PrimeiroComponenteVue from './components/PrimeiroComponente.vue';
+export default {
+  name: 'App',
+  components: {
+    PrimeiroComponenteVue
+  }
+}
+</script>
---
+++ src/components/PrimeiroComponente.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Olá Vue!</h1>
+    <p>O seu nome é: {{ nome }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PrimeiroComponente",
+  data() {
+    return {
+      nome: "Giovana",
+    };
+  },
+};
+</script>
+ 
